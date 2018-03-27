7316575e1210dbf2022ad8b6a85cc19136a610e7
GODueol <weogjw01@naver.com>
2018-03-27 15:07:43 +0900

    edit] 공지사항

--- src/components/Notice.vue
+++ src/components/Notice.vue
@@ -1,15 +1,15 @@
 <template>
 
   <div class="container">
-    <h2>Todo List</h2>
+    <h2>{{userId}}</h2>
     <div class="input-group" style="margin-bottom:10px;">
       <input type="text" class="form-control"
              placeholder="할일을 입력하세요"
-             v-model="name"
-             v-on:keyup.enter="createTodo(name)">
+             v-model="text"
+             v-on:keyup.enter="createTodo(text)">
       <span class="input-group-btn">
 		<button class="btn btn-default" type="button"
-            @click="createTodo(name)">추가</button>
+            @click="createTodo(text)">추가</button>
 	</span>
     </div>
     <ul class="list-group">
@@ -31,23 +31,31 @@
 </template>
 
 <script>
+  import firebase from 'firebase'
+
+  let config = {
+    apiKey: "AIzaSyBvt4z9T0Db3aM-1tCocjrBbC62Ch6QY-8",
+    authDomain: "core-865fc.firebaseapp.com",
+    databaseURL: "https://core-865fc.firebaseio.com",
+    projectId: "core-865fc",
+    storageBucket: "core-865fc.appspot.com",
+    messagingSenderId: "699214818051"
+  };
+  let app = firebase.initializeApp(config);
+  let db = app.database();
+  let noticeRef = db.ref('notice');
 
   export default {
+
     name: 'Notice',
-    methods:{
-      deleteTodo(i){
-        this.todos.splice(0,1);
-      },
-      createTodo(name){
-        if(name != null){
-          this.todos.push({name:name});
-          this.name = null
-        }
-      }
-    },
     data() {
       return {
         msg: 'Welcome to Your Vue.js App',
+        photo: '',
+        userId: '',
+        name: '',
+        email: '',
+        user: {},
         todos: [
           {
             name: '청소'
@@ -63,7 +71,31 @@
           }
         ]
       }
-    }
+    },
+    created: function () {
+      this.user = firebase.auth().currentUser
+      if (this.user) {
+        this.name = this.user.displayName
+        this.email = this.user.email
+        this.photo = this.user.photoURL
+        this.userId = this.user.uid
+      }
+    },
+    methods: {
+      deleteTodo(i) {
+        this.todos.splice(0, 1);
+      },
+      createTodo(name) {
+        if (name != null) {
+          this.todos.push({name: name});
+          this.text = null;
+
+         noticeRef.set({
+            username: name
+          });
+        }
+      }
+    },
   }
 </script>
 
