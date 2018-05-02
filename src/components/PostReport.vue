85e58a1032435e0cb9b51ab95748cafc61734d66
GODueol <weogjw01@naver.com>
2018-05-02 22:25:30 +0900

    edit] TeamCore 메세지 초안

--- src/components/PostReport.vue
+++ src/components/PostReport.vue
@@ -265,6 +265,47 @@
       // TODO : 메세지 보내기 => 요부분은 주열이가 해야할듯..
       sendMessge(targetUuid, msg){
 
+        const messaging = firebase.messaging()
+
+        let chatRef = db.ref('chat');
+        let newChatKey;
+        let chatRoomRef = db.ref('chatRoomList/'+targetUuid);
+        let timestamp = new Date().getTime();
+
+        chatRoomRef.child('TeamCore').once('value').then(function (snapshot) {
+          if(snapshot.val()){
+            // 이미 코어팀한테 메세지를 받은적이 있을때
+            newChatKey = snapshot.val().chatRoomid;
+
+            chatRoomRef.child('TeamCore').set({
+              lastChatTime : timestamp,
+              lastChat : msg
+            })
+          }else{
+            // 코어팀한테 메세지를 받은적이 없을때
+
+            // 키 생성
+            newChatKey = chatRef.push().key;
+            // 방생성
+            chatRoomRef.child('TeamCore').set({
+              badgeCount : 0,
+              chatRoomid : newChatKey,
+              lastChatTime : timestamp,
+              lastViewTime : 0,
+              targetUuid: "TeamCore",
+              lastChat : msg
+            })
+          }
+          chatRef.child(newChatKey).push({
+            check : 1,
+            content : msg,
+            isImage : 0,
+            nickname : "TeamCore",
+            time : timestamp,
+            writer : "TeamCore"
+          })
+        })
+
       }
     }
   }
